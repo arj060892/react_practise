5d4e16db2a1992c512549118d84269c8538184d3
akhil <arj060892@gmail.com>
2018-11-20 02:50:56 +0530

    login page validation and loading

--- src/app/pages/Login.js
+++ src/app/pages/Login.js
@@ -5,79 +5,138 @@ import {
   View,
   StatusBar,
   Image,
-  Alert,
   TextInput,
-  TouchableHighlight
+  TouchableHighlight,
+  ActivityIndicator
 } from "react-native";
 
-const logo = require("../../images/login-logo.png");
-const loginText = "Login";
-const signup = "Signup";
 export default class LoginScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "User Name",
-      password: "Password",
+      username: "",
+      password: "",
       isUsernameFocused: false,
-      isPasswordFocused: false
+      isPasswordFocused: false,
+      usernameValid: true,
+      passwordValid: true,
+      userValid: false,
+      isLoggingIn: false
     };
   }
-  onFocusChangeUsername = () => {
-    this.setState({ isUsernameFocused: true });
-  };
-  onFocusChangePassword = () => {
-    this.setState({ isPasswordFocused: true });
-  };
+
+  onFocusActive(type) {
+    if (type == "username") {
+      this.setState({ isUsernameFocused: true });
+    } else if (type == "password") {
+      this.setState({ isPasswordFocused: true });
+    }
+  }
+  userLogin() {
+    this.setState({ isLoggingIn: true });
+  }
+  validate(text, type) {
+    usernameRegx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    passwordRegx = /^\S+$/;
+    if (type == "username") {
+      if (usernameRegx.test(text)) {
+        this.setState({ usernameValid: true });
+      } else {
+        this.setState({ usernameValid: false });
+      }
+    } else if (type == "password") {
+      if (passwordRegx.test(text)) {
+        this.setState({ passwordValid: true });
+      } else {
+        this.setState({ passwordValid: false });
+      }
+    }
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" backgroundColor="#388e3c" />
-        <Image source={logo} style={styles.logo} />
+        <Image
+          source={require("../../images/login-logo.png")}
+          style={styles.logo}
+        />
         <TextInput
-          placeholder={this.state.username}
-          onFocus={() => this.onFocusChangeUsername()}
-          style={
+          editable={!this.state.isLoggingIn}
+          selectTextOnFocus={!this.state.isLoggingIn}
+          placeholder="User Name"
+          onFocus={() => this.onFocusActive("username")}
+          onChangeText={username => this.validate(username, "username")}
+          style={[
             this.state.isUsernameFocused
               ? styles.inputTextActive
-              : styles.inputTextGrey
-          }
+              : styles.inputTextGrey,
+            !this.state.usernameValid ? styles.inputTextError : null
+          ]}
         />
+        {!this.state.usernameValid ? <Text>Username invalid!</Text> : null}
 
         <TextInput
           secureTextEntry={true}
-          onFocus={() => this.onFocusChangePassword()}
-          placeholder={this.state.password}
-          style={
+          editable={!this.state.isLoggingIn}
+          selectTextOnFocus={!this.state.isLoggingIn}
+          onFocus={() => this.onFocusActive("password")}
+          onChangeText={text => this.validate(text, "password")}
+          placeholder="Password"
+          style={[
             this.state.isPasswordFocused
               ? styles.inputTextActive
-              : styles.inputTextGrey
-          }
+              : styles.inputTextGrey,
+            !this.state.passwordValid ? styles.inputTextError : null
+          ]}
         />
+        {!this.state.passwordValid ? <Text>Password invalid!</Text> : null}
 
-        <View style={{ flexDirection: "row", width: "80%", marginTop: 20 }}>
-          <View style={{ width: "50%" }}>
-            <TouchableHighlight
-              onPress={() => this.props.navigation.navigate("Register")}
-              style={styles.roundedCorners}
-              underlayColor="#188b0c"
-            >
-              <Text
-                style={[
-                  styles.roundedCorners,
-                  styles.textButtonLight,
-                  styles.marginRight
-                ]}
+        {this.state.isLoggingIn ? (
+          <ActivityIndicator
+            animating={this.state.isLoggingIn}
+            color="#188b0c"
+            size="large"
+          />
+        ) : (
+          <View style={{ flexDirection: "row", width: "80%", marginTop: 20 }}>
+            <View style={{ width: "50%" }}>
+              <TouchableHighlight
+                onPress={() => this.props.navigation.navigate("Register")}
+                style={styles.roundedCorners}
+                underlayColor="#188b0c"
               >
-                {signup}
-              </Text>
-            </TouchableHighlight>
+                <Text
+                  style={[
+                    styles.roundedCorners,
+                    styles.textButtonLight,
+                    styles.marginRight
+                  ]}
+                >
+                  SignUp
+                </Text>
+              </TouchableHighlight>
+            </View>
+
+            <View style={{ width: "50%" }}>
+              <TouchableHighlight
+                style={styles.roundedCorners}
+                underlayColor="#188b0c"
+                onPress={this.userLogin.bind(this)}
+              >
+                <Text
+                  style={[
+                    styles.roundedCorners,
+                    styles.text,
+                    styles.marginLeft
+                  ]}
+                >
+                  Login
+                </Text>
+              </TouchableHighlight>
+            </View>
           </View>
-          <View style={{ width: "50%" }}>
-            <Text style={[styles.text, styles.marginLeft]}>{loginText}</Text>
-          </View>
-        </View>
+        )}
       </View>
     );
   }
@@ -129,6 +188,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 18
   },
+  inputTextError: {
+    borderBottomColor: "red",
+    borderBottomWidth: 1
+  },
   inputTextActive: {
     width: "90%",
     borderBottomColor: "#fe9a44",
@@ -144,5 +207,10 @@ const styles = StyleSheet.create({
   },
   marginRight: {
     marginRight: 10
+  },
+  activityIndicator: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
